f32c07b32424d37b0961b446cfa3fabaebe08613
noura2tarek <Nourat536@gmail.com>
2023-12-31 18:34:52 +0200

    Improve performance

--- lib/core/controllers/note_cubit/note_cubit.dart
+++ lib/core/controllers/note_cubit/note_cubit.dart
@@ -39,9 +39,11 @@ class NoteCubit extends Cubit<NoteState> {
   updateDatabase({required Note note}) {
     emit(NoteUpdateDataLoadingState());
     sqlDB.updateDB(note: note).then((value) {
-      print("updated successfully======");
-      emit(NoteUpdateDataSuccessState());
-      readDatabase();
+      if (value! > 0) {
+        emit(NoteUpdateDataSuccessState());
+        print("updated successfully======");
+        readDatabase();
+      }
     }).catchError((error) {
       emit(NoteUpdateDataErrorState());
     });
@@ -49,33 +51,39 @@ class NoteCubit extends Cubit<NoteState> {
 
   /*--------------------------  Read all Notes -----------------------------*/
   List<Note> notes = [];
+
   readDatabase() {
     sqlDB.readDB().then((value) {
-      notes = value;
+      notes = value; // add list in a list
       emit(NoteReadDataSuccessState());
     }).catchError((error) {
       emit(NoteReadDataErrorState());
     });
   }
+
   /*--------------------------  Search Note -----------------------------*/
   //ask here
   List<Note> notesSearch = [];
+
   searchInDatabase({required Note note}) {
     sqlDB.readDB(where: "id = 2").then((value) {
-      notesSearch = value;
+      notesSearch = value; // add list in a list
       emit(NoteSearchDataSuccessState());
     }).catchError((error) {
       emit(NoteSearchDataErrorState());
     });
   }
 
-
   /*--------------------------  Delete Note -----------------------------*/
   deleteFromDatabase({required int id}) {
     sqlDB.deleteFromDB(id: id).then((value) {
-      print("row  with id = $id deleted successfully======");
-      emit(NoteDeleteDataSuccessState());
-      readDatabase();
+      //check if value of response is not zero to avoid errors, that means the function completed normally.
+      if (value! > 0) {
+        emit(NoteDeleteDataSuccessState());
+        print("row with id = $id deleted successfully======");
+        // Delete the element from the list notes if it fulfills the condition.
+        notes.removeWhere((element) => element.id == id);
+      }
     }).catchError((error) {
       emit(NoteDeleteDataErrorState());
     });
